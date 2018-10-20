42e8b70d3ab2f2da6b1b3ba008ffaa3ce8c589a9
yobs0814 <yobs0814@gmail.com>
2018-10-20 20:23:10 +0900

    upload sources

---
+++ SWExpert/P15683/cmake-build-debug/CMakeFiles/P15683.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P15683.dir/main.cpp.o"
+  "P15683.pdb"
+  "P15683"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P15683.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
